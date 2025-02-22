e8193b685d2f110b9bd567ecc8d35645398215c1
Onyinsi <onyinsisoss@outlook.com>
2025-02-22 23:42:31 +0300

    .

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { RouterProvider,createBrowserRouter } from 'react-router-dom'
+import { RouterProvider,HashRouter } from 'react-router-dom'
 import Contacts from './routes/contacts.jsx'
 import Abouut from './routes/abouut.jsx'
 import Services from './routes/services.jsx'
 import Gallery from './routes/gallery.jsx'
 
-const router = createBrowserRouter([
+const router = HashRouter([
   {path: '/', element:< App />},
   {path: '/abouut', element:< Abouut />},
   {path: '/contacts', element:< Contacts />},
